86edc574c0c1cc43a9ca9abb19535b9818ca0003
CyranHOEdeBergerac <danael.carbonneau@protonmail.com>
2022-11-21 00:52:47 +0100

    Ménage

--- Experimentations/tests_manipulation_fichiers.c
+++
@@ -1,9 +0,0 @@
-#include "Manipulation_fichiers_genomes.h"
-
-
-int main(){
-    Nom_taille_x** tableau_noms_fichiers = lire_noms_fichiers("../Instances_genome");
-    for(int i = 0 ; i < NOMBRE_FICHIERS_LUS_MAX ; i ++){
-        printf("%s avec taille de x = %d\n",tableau_noms_fichiers[i]->nom,tableau_noms_fichiers[i]->taille_x);
-    }
-}
