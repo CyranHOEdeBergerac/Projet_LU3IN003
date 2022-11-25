b7a66e48916449a356c2b0963d7dc11d0b49915c
CyranHOEdeBergerac <danael.carbonneau@protonmail.com>
2022-11-26 00:26:29 +0100

    taille tableau

--- Programmation_dynamique/test_tache_D.c
+++ Programmation_dynamique/test_tache_D.c
@@ -5,10 +5,10 @@ int main(){
     int** Dist = (int**) malloc(2*sizeof(int*));
     int** I = (int**) malloc(2 * sizeof(int*));
 
-    Dist[0] = (int*) malloc(10*sizeof(int));
-    Dist[1] = (int*) malloc(10*sizeof(int));
-    I[0] = (int*) malloc(10*sizeof(int));
-    I[1] = (int*) malloc(10*sizeof(int));
+    Dist[0] = (int*) malloc(11*sizeof(int));
+    Dist[1] = (int*) malloc(11*sizeof(int));
+    I[0] = (int*) malloc(11*sizeof(int));
+    I[1] = (int*) malloc(11*sizeof(int));
 
     Alignement* resultat = SOL_2("AGTACGCA","ATCGTATGC",9,10,Dist,I);
 
